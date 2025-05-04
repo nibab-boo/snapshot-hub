9841324abcb088ef5c21d64681e38cfdd8fc923f
Babin Bohora <bohorababin1@gmail.com>
2025-05-03 23:59:23 -0400

    Removed Babel and used swr for testing with jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
-module.exports = {
-    transform: { "^.+\\.js$": "babel-jest" },
-    testEnvironment: "jsdom",
-  };
+const nextJest = require('next/jest');
+const createJestConfig = nextJest({dir: './'});
+
+/** @type {import('jest').Config} */
+
+const customConfig = {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
+
+module.exports = createJestConfig(customConfig);
